a39f6abc455efe56716068feff516a9438aa8a59
G00407454 <g00407454@atu.ie>
2022-10-27 19:12:33 +0100

    Locker for names and details

---
+++ src/main/java/Locker.java
@@ -0,0 +1,40 @@
+public class Locker {
+
+        private  String Name;
+        private  String Number;
+        private  String Email;
+        private  String Course;
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getNumber() {
+        return Number;
+    }
+
+    public void setNumber(String number) {
+        Number = number;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public String getCourse() {
+        return Course;
+    }
+
+    public void setCourse(String course) {
+        Course = course;
+    }
+}
+
